37877f814d1e4616b70e389218d079aba3a40f39
Adam Aron Edelsten <adamedelsten@hotmail.com>
2019-03-13 13:31:50 +0100

    Merge origin/master
    Conflicts:
    	src/ftp/FTP.java

--- src/grafiskbilletautomat/billetAutomatKlasse.java
+++ src/grafiskbilletautomat/billetAutomatKlasse.java
@@ -2,6 +2,7 @@ package grafiskbilletautomat;
 
 import java.text.SimpleDateFormat;
 import java.time.*;
+import java.util.ArrayList;
 import java.util.Date;
 
 
@@ -11,6 +12,100 @@ prisen bliver bestemt herfra
 */
 public class billetAutomatKlasse
 {
+    private ArrayList<String> transaktioner = new ArrayList<String>();
+
+    private double pris;    // Prisen for én billet.
+    private double balance; // Hvor mange penge kunden p.t. har puttet i automaten
+    private int antalBilletterSolgt; // Antal billetter automaten i alt har solgt
+
+    /**
+     * Opret en billetautomat, der sælger billetter til en given billetpris.
+     *o
+     * @param billetpris skal være større end nul (p.t. bliver det ikke tjekket)
+     */
+
+    /**
+     * Giver prisen for en billet.
+     */
+    public double getBilletpris()
+    {
+        double resultat = pris;
+        return resultat;
+    }
+
+    /**
+     * Modtag nogle penge (i kroner) fra en kunde.
+     */
+    public void indsætPenge(double beløb)
+    {
+        Date tid = new Date();
+	transaktioner.add(tid + " der blev indsat "+beløb+" kroner");
+        balance = balance + beløb;
+    }
+
+    /**
+     * Giver balancen (beløbet maskinen har modtaget til den næste billet).
+     */
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    /**
+     * Udskriv en billet.
+     */
+    public void udskrivBillet()
+    {
+        if (balance >= pris)
+        {
+            transaktioner.add(new Date() + " der blev udskrevet en billet");
+            antalBilletterSolgt = antalBilletterSolgt + 1;
+            balance = balance - pris;
+
+            System.out.println("##########B##T##########");
+            System.out.println("# Borgen Trafikselskab #");
+            System.out.println("#                      #");
+            System.out.println("#        Billet        #");
+            System.out.println("#        " + pris + " kr.        #");
+            System.out.println("#                      #");
+            System.out.println("# Du har " + balance + " kr til gode #");
+            System.out.println("##########B##T##########");
+            System.out.println();
+        } else
+        {
+            System.out.println("Saldo for lav");
+        }
+    }
+    
+    	public double returpenge() {
+		double returbeløb = balance;
+		transaktioner.add(new Date() + " kunden fik "+returbeløb+" kr retur");
+		balance = 0;
+		System.out.println("Du får "+returbeløb+" kr retur");
+		return returbeløb;
+	}
+
+    
+    //Service metoder:
+
+    public void setBilletpris(String montørkode, double nyPris)
+    {
+        if (montørkode.equals("1234"))
+        {
+            pris = nyPris;
+        } else
+        {
+            System.err.println("Kunne ikke sætte pris - forkert kode");
+        }
+    }
+
+    public double getSamletSalgsbeløb(String montørkode)
+    {
+        if (montørkode.equals("1234"));
+        return pris * antalBilletterSolgt;
+    }
+    
+    
     public static String getDate()
     {
         Date epoch = new Date(System.currentTimeMillis());
