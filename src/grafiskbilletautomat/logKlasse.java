6127854d21fe3664de1074ea7efb7f9017563369
FK <FK@WINDOWS-KEBDGBI>
2019-03-15 01:20:30 +0100

    start på log klassen

--- src/grafiskbilletautomat/logKlasse.java
+++ src/grafiskbilletautomat/logKlasse.java
@@ -8,5 +8,9 @@ package grafiskbilletautomat;
 
 public class logKlasse
 {
-    
+    //Tid, dato, beskrivelse + type id, beløb, fortløbende nummer (id)
+    logKlasse()
+    {
+            
+    }
 }
